69f5460b5a005c760716ad83258dc0d7ed305c06
Josh Malone <jmalone@nrao.edu>
2018-02-23 14:06:33 -0500

    More robust lfs parsing

--- Lustre/check_lustre.pl
+++ Lustre/check_lustre.pl
@@ -2,19 +2,35 @@
 #
 # Nagios check plugin for lustre filesystems.
 #
-# Josh Malone (jmalone@nrao.edu) - July 2013
+# Copyright (c) 2016 - Joshua Malone
 #
-# Revision 3 - July 17, 2013
-# Revision 4 - July 18, 2013
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
 
-$revision=4;
+$revision=5;
 
 use lib "/usr/lib64/nagios/plugins";
 use lib "/usr/lib/nagios/plugins";
 use lib ".";
 use utils qw($TIMEOUT %ERRORS &print_revision &support);
 use Switch;
-use Getopt::Std;
+use Getopt::Long;
 
 $exstate=$ERRORS{'OK'};
 $message="OK: All filesystems okay";
@@ -27,9 +43,28 @@ $critpct=85;
 
 ##########
 
-getopts('w:c:htsvf:l:p:d');
-if ($opt_w) { $warnpct=$opt_w; }
-if ($opt_c) { $critpct=$opt_c; }
+GetOptions(
+    "f|filesystem=s"	=> \$opt_f,
+    "p|pool=s"		=> \$opt_p,
+    "w|warning=i"	=> \$warnpct,
+    "c|critical=i"	=> \$critpct,
+    "h|help"		=> \$opt_h,
+    "t|ignore-unavailable"	=> \$opt_t,
+    "l|log"		=> \$logfile,
+    "v|verbose"		=> \$verbose,
+    "V|version"		=> \$print_version,
+    "s"			=> \$opt_s,
+    "i|ignore-ost=s"	=> \@ignore_osts,
+    "ignore-regex=s"	=> \$ignore_regex,
+);
+
+# Convert the list of ignored OSTs into a hash
+%ignore_these = map { $_ => 1 } @ignore_osts;
+
+# Build regex to ignore
+if ($ignore_regex) {
+    $ignore_match = qr/$ignore_regex/;
+}
 
 if ($opt_v) {
     printf("check_lustre.pl, by jmalone\@nrao.edu, version %d\n", $revision);
@@ -38,7 +73,7 @@ if ($opt_v) {
 if ($opt_h) {
     # Help message
     print << "EOHELP";
-Usage:  check_lustre.pl [-tvh] [-f <fs>] [-p <pool>]  [-w <warn>] [-c <crit>] [-l <logfile>]
+Usage:  check_lustre.pl [-tvVh] [-f <fs>] [-p <pool>]  [-w <warn>] [-c <crit>] [-l <logfile>]
 
 	-f: Only check a single lustre filesystem
 	-p: Only check a single lustre pool
@@ -46,20 +81,25 @@ Usage:  check_lustre.pl [-tvh] [-f <fs>] [-p <pool>]  [-w <warn>] [-c <crit>] [-
 	-c: Critical space threshold (percent used)
 	-t: Ignore OSTs that are temporarily unavailable
 	-l: Write debug log information to file
-	-v: Print version information
+	-V: Print version information
+	-v: Print verbose progress information
 	-h: Print this help message
+
+	-i|--ignore-ost:  Ignore problems with a specific OST
+	--ignore-regex:   Ignore OSTs matching given regular expression
+
 EOHELP
     exit 0;
 }
-if ($opt_l) {
-    open(LOG, ">>", $opt_l) or die ("Unable to open logfile $opt_l\n");
+if ($logfile) {
+    open(LOG, ">>", $logfile) or die ("Unable to open logfile $logfile\n");
 }
-print LOG "Starting check_lustre.pl at ".localtime()."\n" if ($opt_l);
+print LOG "Starting check_lustre.pl at ".localtime()."\n" if ($logfile);
 
 # Set alarm to time out possibly hung `lfs df` command
 alarm 40;
 $SIG{ALRM} = sub {
-    print LOG "CRITICAL - check_lustre.pl quit after ALARM at ".localtime()."\n" if ($opt_l);
+    print LOG "CRITICAL - check_lustre.pl quit after ALARM at ".localtime()."\n" if ($logfile);
     print "CRITICAL: lustre lfs operation timed out\n";
     exit $ERRORS{'CRITICAL'};
 };
@@ -70,7 +110,7 @@ if ( ! -d "/proc/fs/lustre" ) {
     exit $ERRORS{'CRITICAL'};
 }
 
-print LOG "Checking /proc/fs/lustre/health_check at ".localtime()."\n" if ($opt_l);
+print LOG "Checking /proc/fs/lustre/health_check at ".localtime()."\n" if ($logfile);
 open (PROC, "/proc/fs/lustre/health_check") or die("Cannot read lustre health_check - is lustre installed?");
 while(<PROC>) {
     chomp();
@@ -80,7 +120,7 @@ while(<PROC>) {
     }
 }
 close (PROC);
-print LOG "Proc check completed at ".localtime()."\n" if ($opt_l);
+print LOG "Proc check completed at ".localtime()."\n" if ($logfile);
 
 ##################################################
 
@@ -98,39 +138,41 @@ if ($opt_s) {
 while (<LFS>) {
     chomp();
     @parts=split(' ');
-    switch ($_) {
-	case /Resource temporarily unavailable/ {
-		next unless ($opt_t);
+    # Check to see if we should ignore this particular ost
+    next if ( $ignore_these{$parts[0]} );
+    if ( /unavailable/ ) {
+        if ( $opt_t && /Resource temporarily unavailable/) {
+	    next;
+	} elsif ( $ignore_regex && $parts[0] =~ $ignore_match) {
+	    next;
+	} else {
+	    $exstate=$ERRORS{'CRITICAL'};
+	    $message="CRITICAL: ".join(' ',@parts);
 	}
-	case /unavailable/ {
-		$exstate=$ERRORS{'CRITICAL'};
-		$message="CRITICAL: ".join(' ',@parts);
-	}
-	case /filesystem summary/ {
-		if (!$opt_f || $_ =~ /$opt_f$/) {
-		    break if ($opt_p && ($parts[4]==0) );
-		    $fs=$parts[6];
-		    $fs=~s/.*\///;
-		    $btotal{$fs}=$parts[2]*1024;
-		    $bused{$fs}=$parts[3]*1024;
-		    $tmp=$parts[5];
-		    $tmp=~s/\%//;
-		    $pctused{$fs}=$parts[5];
-		    if ($parts[5] > $critpct) {
-			# Don't overwrite a more serious OST error
-			if( $exstate==$ERRORS{'OK'} ) {
-			    $message=sprintf("CRITICAL: Filesystem %s is %d%% full", $fs, $parts[5]);
-			    $exstate=$ERRORS{'CRITICAL'};
-			}
-		    }
-		    if ($parts[5] > $warnpct) {
-			if( $exstate==$ERRORS{'OK'} ) {
-			    $message=sprintf("WARNING: Filesystem %s is %d%% full", $fs, $parts[5]);
-			    $exstate=$ERRORS{'WARNING'};
-			}
-		    }
-		print LOG "FS summary at ".localtime()." - $_\n" if ($opt_l);
+    } elsif ( /filesystem summary/) {
+	if (!$opt_f || $_ =~ /$opt_f$/) {
+	    break if ($opt_p && ($parts[4]==0) );
+	    $fs=$parts[6];
+	    $fs=~s/.*\///;
+	    $btotal{$fs}=$parts[2]*1024;
+	    $bused{$fs}=$parts[3]*1024;
+	    $tmp=$parts[5];
+	    $tmp=~s/\%//;
+	    $pctused{$fs}=$parts[5];
+	    if ($parts[5] > $critpct) {
+		# Don't overwrite a more serious OST error
+		if( $exstate <= $ERRORS{'WARNING'} ) {
+		    $message=sprintf("CRITICAL: Filesystem %s is %d%% full", $fs, $parts[5]);
+		    $exstate=$ERRORS{'CRITICAL'};
 		}
+	    }
+	    if ($parts[5] > $warnpct) {
+		if( $exstate==$ERRORS{'OK'} ) {
+		    $message=sprintf("WARNING: Filesystem %s is %d%% full", $fs, $parts[5]);
+		    $exstate=$ERRORS{'WARNING'};
+		}
+	    }
+	print LOG "FS summary at ".localtime()." - $_\n" if ($logfile);
 	}
     }
 }
